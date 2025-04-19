578b10a73ab2ac648746f004c5a40b8cada2d5f5
Tanke6003 <Ruben.Farias.1999@Outlook.es>
2025-04-19 16:10:09 -0600

    [UPDATE] HomeScreen Implemented Menu option without navigation.

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,9 +11,49 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + material 3'),
      
-      )
-      
+      ),
+      body: _HomeView(),
     
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+    itemCount: menuItems.length,
+    itemBuilder: 
+      (context, index) {
+        final menuItem = menuItems[index];
+        return _CustomListTile( menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile(
+  {  required this.menuItem
+ } );
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(menuItem.icon,color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_outlined,color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        //TODO: Navigate to the respective screen
+      },
     );
   }
 }
